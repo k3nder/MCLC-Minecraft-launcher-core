a5bc18e1997cedfe369d624dd731bd1c3b5a3ab4
kederdev <kristianzvezdakris@gmail.com>
2023-08-28 14:44:20 +0200

    import

---
+++ src/main/java/net/kender/MCLC/Constants.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.kender.MCLC;
+
+/**
+ *
+ * @author krist
+ */
+public class Constants {
+    public static final String OS = System.getProperty("os.name");
+    public static final String User = System.getProperty("user.name");
+    public static String command = "";
+    public static int JavaV;
+    public static String gameDir = "C:\\Users\\" + User + "\\AppData\\Roaming\\.minecraft";
+
+    public static String MinecraftPath = "C:\\Users\\" + Constants.User + "\\AppData\\Roaming\\.minecraft";
+    public static String localPath = "C:\\Users\\" + Constants.User + "\\Documents\\LauncherTrinity";
+
+    public static String documents = "C:\\Users\\" + User + "\\Documents\\";
+
+    public static String version = "";
+
+    public static String JRE8w = "C:\\Users\\" + User + "\\AppData\\Local\\Temp\\JREs_LauncherTrinity";
+    public static String JRE20w = "C:\\Users\\" + User + "\\AppData\\Local\\Temp\\JREs_LauncherTrinity";
+
+    
+}
---
+++ src/main/java/net/kender/MCLC/test/example.java
@@ -0,0 +1,87 @@
+package net.kender.MCLC.test;
+
+import net.kender.MCLC.JsonUtils;
+import net.kender.MCLC.UtilsFiles;
+import net.kender.MCLC.argsGenerator;
+
+import java.io.IOException;
+
+import static net.kender.MCLC.Constants.MinecraftPath;
+import static net.kender.MCLC.JsonUtils.*;
+import static net.kender.MCLC.UtilsFiles.*;
+import static net.kender.MCLC.argsGenerator.genrateArgsCommand;
+import static net.kender.MCLC.argsGenerator.getJREVersion;
+import static net.kender.MCLC.assetsDowloader.AssetsDowload;
+
+/*
+* this ia a example
+*
+* not download assest
+*
+* wiki in:
+*
+*
+*
+*
+* */
+public class example {
+    public static String version = "1.20.1";
+    public static String Username = "";
+    public static void main(String[] args) throws IOException {
+
+        //get Lis of Versions dowloables
+        String[] listOfVersionsDownloadble = new String[0];
+        try {
+            listOfVersionsDownloadble = GetListOfVersions("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int i = listOfVersionsDownloadble.length;
+        int it = 0;
+        while(i != it){
+            System.out.println(listOfVersionsDownloadble[it]);
+            it++;
+        }
+
+        //get Json Of Version URL
+        String UrlOfVersion = GetUrlOfJsonOfVersion("https://launchermeta.mojang.com/mc/game/version_manifest.json", "1.20.1");
+        System.out.println(UrlOfVersion);
+
+        //read the Json of version
+
+        String ContentOfJsonOfVersion = getJsonVersion(UrlOfVersion);
+        System.out.println(ContentOfJsonOfVersion);
+
+        //create the file Json
+        createFile(MinecraftPath + "\\example\\" + version,"json", version, ContentOfJsonOfVersion);
+
+        //get the url of the client
+        String UrlOfClient = readJsonVersion(  MinecraftPath + "\\example\\" + version + "\\" + version + ".json");
+
+        //dowload the client
+        download(UrlOfClient,MinecraftPath + "\\example\\" + version + "\\",version,".jar");
+
+        //get libraries of minecraft
+        getLibs(MinecraftPath + "\\example\\" + version + "\\" + version + ".json",version, MinecraftPath + "\\example\\libs\\" + version);
+
+        String assetsIndexUrl = ReadFileAssetsIndex(  MinecraftPath + "\\example\\" + version + "\\" + version + ".json");
+
+        String assestsContent = getJsonVersion(assetsIndexUrl);
+
+        createFile( MinecraftPath + "\\assets\\indexes", "json", JsonUtils.getAssesNum(MinecraftPath + "\\example\\" + version + "/" + version + ".json"), assestsContent);
+
+        AssetsDowload(MinecraftPath + "\\assets\\indexes\\" + JsonUtils.getAssesNum(MinecraftPath + "\\example\\" + version + "\\" + version + ".json") + ".json", MinecraftPath + "\\assets\\virtual\\legacy\\", MinecraftPath + "\\assets\\objects");
+        //get te version of the jre(this is a downloader of the jre at a time)
+        getJREVersion(version,MinecraftPath + "\\example\\" + version + "\\" + version + ".json", MinecraftPath + "\\example\\bin");
+
+        //get main class
+        String mainClass = getMainClass(MinecraftPath + "\\example\\" + version + "\\" + version + ".json");
+
+        //get the command for launch minecraft                                                                                                                                                                                                  gamedir       mainclass
+        String command = genrateArgsCommand(version,MinecraftPath + "\\example\\libs\\" + version + "\\" + version,MinecraftPath + "\\example\\" + version + "\\" + version + ".jar",Username,"","0",MinecraftPath,mainClass,version,false,version);
+
+        //execute the command
+        run(command);
+    }
+}
